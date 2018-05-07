89d374dcd35f68548e4b786e2cb7a8bc3dafe70c
m <marcosjbarroso82@gmail.com>
2018-05-07 11:53:44 -0300

    classification

--- regression.py
+++ regression.py
@@ -24,6 +24,16 @@ X_test = sm.apply_preprocess_steps(X_test)
 # TODO: Implement the other models
 if sm.get_action() == 'regression':
     model = Regressor(sm.get_model(), sm.get_model_conf())
+elif sm.get_action() == 'classification':
+    # TODO: Rename class for both Regression and Classification
+    model = Regressor(sm.get_model(), sm.get_model_conf())
+    
+    # TODO: Move Feature Scaling to logic
+    # Feature Scaling
+    from sklearn.preprocessing import StandardScaler
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
     
 model.fit(X_train, y_train)
 
@@ -34,8 +44,47 @@ y_pred = model.predict(X_test)
 sm.save()
 prev_session = sm.name
 
-# Plot
-# plot_2d_regression(regressor, X_train, y_train)
-# plot_2d_regression(regressor, X_test, y_test)
-
+if sm.get_action() == 'regression':
+    # Plot
+    # plot_2d_regression(regressor, X_train, y_train)
+    # plot_2d_regression(regressor, X_test, y_test)
+    pass
+elif sm.get_action() == 'classification':
+    # TODO: Move Graphics to helpers
+    
+    # Visualising the Training set results
+    from matplotlib.colors import ListedColormap
+    X_set, y_set = X_train, y_train
+    X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                         np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                 alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    for i, j in enumerate(np.unique(y_set)):
+        plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                    c = ListedColormap(('red', 'green'))(i), label = j)
+    plt.title('Logistic Regression (Training set)')
+    plt.xlabel('Age')
+    plt.ylabel('Estimated Salary')
+    plt.legend()
+    plt.show()
+    
+    # Visualising the Test set results
+    from matplotlib.colors import ListedColormap
+    X_set, y_set = X_test, y_test
+    X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                         np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                 alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    for i, j in enumerate(np.unique(y_set)):
+        plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                    c = ListedColormap(('red', 'green'))(i), label = j)
+    plt.title('Logistic Regression (Test set)')
+    plt.xlabel('Age')
+    plt.ylabel('Estimated Salary')
+    plt.legend()
+    plt.show()
 
